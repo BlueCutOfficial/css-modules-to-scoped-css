d41756cfb837a77ee66195dc690569c7691419d5
Marine Dunstetter <bluecut.official@gmail.com>
2025-03-12 15:36:34 +0100

    use the programmatic styles access feature of ember-css-modules

--- tests/integration/components/welcome-page-copy-test.js
+++ tests/integration/components/welcome-page-copy-test.js
@@ -3,11 +3,14 @@ import { setupRenderingTest } from 'css-modules-to-scoped-css/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
+import styles from 'css-modules-to-scoped-css/components/welcome-page-copy.css';
+
 module('Integration | Component | welcome-page-copy', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     await render(hbs`<WelcomePageCopy />`);
-    assert.dom().includesText('Congratulations, you made it!');
+    assert.dom('#title').includesText('Congratulations, you made it!');
+    assert.dom('#title').hasAttribute('class', styles['welcome-title']);
   });
 });

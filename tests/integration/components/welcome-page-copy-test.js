4d656824a1fc9e6efe56067843ad853230a38b3e
Marine Dunstetter <bluecut.official@gmail.com>
2025-03-12 15:45:37 +0100

    use the value feature of ember-css-modules

--- tests/integration/components/welcome-page-copy-test.js
+++ tests/integration/components/welcome-page-copy-test.js
@@ -12,5 +12,10 @@ module('Integration | Component | welcome-page-copy', function (hooks) {
     await render(hbs`<WelcomePageCopy />`);
     assert.dom('#title').includesText('Congratulations, you made it!');
     assert.dom('#title').hasAttribute('class', styles['welcome-title']);
+    assert.equal(
+      styles['ember-orange'],
+      'rgb(255, 92, 68)',
+      'ember-orange color is imported',
+    );
   });
 });

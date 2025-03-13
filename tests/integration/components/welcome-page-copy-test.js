732468ab88150679e184e11b33ca24779b8ac342
Marine Dunstetter <bluecut.official@gmail.com>
2025-03-13 15:02:02 +0100

    Refactor using ember-scoped-css

--- tests/integration/components/welcome-page-copy-test.js
+++ tests/integration/components/welcome-page-copy-test.js
@@ -3,19 +3,19 @@ import { setupRenderingTest } from 'css-modules-to-scoped-css/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-import styles from 'css-modules-to-scoped-css/components/welcome-page-copy.css';
+import { scopedClass } from 'ember-scoped-css/test-support';
 
 module('Integration | Component | welcome-page-copy', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     await render(hbs`<WelcomePageCopy />`);
-    assert.dom('#welcome-id').includesText('Congratulations, you made it!');
-    assert.dom('#welcome-id').hasAttribute('class', styles['welcome']);
-    assert.equal(
-      styles['ember-orange'],
-      'rgb(255, 92, 68)',
-      'ember-orange color is imported',
+    const rewrittenClass = scopedClass(
+      'welcome',
+      'css-modules-to-scoped-css/components/welcome-page-copy',
     );
+
+    assert.dom('#welcome-id').includesText('Congratulations, you made it!');
+    assert.dom('#welcome-id').hasAttribute('class', rewrittenClass);
   });
 });

88f01cd36a21d7315f210ae81338c0841fa0df57
Marine Dunstetter <bluecut.official@gmail.com>
2025-03-18 14:55:11 +0100

    rename css files and configure ember-css-modules

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
+    cssModules: {
+      extension: 'module.css',
+      intermediateOutputPath: 'app/styles/css-modules.css',
+    },
   });
 
   return app.toTree();
